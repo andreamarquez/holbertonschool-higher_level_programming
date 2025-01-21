8eb51e0e31a57235a22ab3ad9987a114bc94df41
andreamarquez <andreamarquezg.95@gmail.com>
2025-01-21 17:48:09 +0100

    code not executed when imported

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from import_add import add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
