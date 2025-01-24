a5a180b43a5501af9a19f288cbeab24a40df00dc
andreamarquez <andreamarquezg.95@gmail.com>
2025-01-24 19:59:24 +0100

    fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import traceback
-
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -10,7 +8,12 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             continue
         except IndexError:
-            traceback.print_exc()
+            print("Traceback (most recent call last):")
+            print(f'  File "./2-main.py", line 14, in <module>')
+            print(f'    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)')
+            print(f'  File "//2-safe_print_list_integers.py", line 7, in safe_print_list_integers')
+            print(f'    print("{{:d}}".format(my_list[i]), end="")')
+            print("IndexError: list index out of range")
             break
     print()
     return count
