2d4d3cc8fd8f249f11a2f47ef6c42e06b048c8da
andreamarquez <andreamarquezg.95@gmail.com>
2025-01-28 12:48:39 +0100

    module documented

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square that represents a square.
+"""
+
 class Square:
+    """
+    This class represents a square with a private size attribute.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
     def __init__(self, size):
+        """
+        Initializes the Square with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size  # private attribute
