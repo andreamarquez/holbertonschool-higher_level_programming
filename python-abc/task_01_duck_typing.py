8d510b552c9f9f0159f862678176cc4afd149b80
andreamarquez <andreamarquezg.95@gmail.com>
2025-02-09 20:34:09 +0100

    abs

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -79,7 +79,7 @@ class Circle(Shape):
         Returns:
             float: The perimeter of the circle.
         """
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * abs(self.radius)
 
 
 class Rectangle(Shape):
@@ -109,7 +109,7 @@ class Rectangle(Shape):
         Returns:
             float: The area of the rectangle.
         """
-        return self.width * self.height
+        return abs(self.width) * abs(self.height)
 
     def perimeter(self):
         """
@@ -118,7 +118,7 @@ class Rectangle(Shape):
         Returns:
             float: The perimeter of the rectangle.
         """
-        return 2 * (self.width + self.height)
+        return 2 * (abs(self.width) + abs(self.height))
 
 
 def shape_info(shape):
