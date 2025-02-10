a065c42d1be94a4341b4c27e70a1ecb99da7e0a4
andreamarquez <andreamarquezg.95@gmail.com>
2025-02-10 18:29:32 +0100

    A function that reads a text file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module provides a function to read the contents of a file and print it.
+
+Functions:
+    read_file(filename=""): Reads the content of the specified file and prints it.
+"""
+
+def read_file(filename=""):
+    """
+    Reads the content of the specified file and prints it.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string.
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
+        read_data = file.read()
+        print(read_data)
