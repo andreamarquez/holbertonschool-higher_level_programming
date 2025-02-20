9df3c811d207978ec510e9849a8372d983f41e05
andreamarquez <andreamarquezg.95@gmail.com>
2025-02-20 18:46:24 +0100

    add data

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,7 +2,12 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {}
+users = {
+    {
+        "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+        "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+    }
+}
 
 @app.route('/')
 def home():
