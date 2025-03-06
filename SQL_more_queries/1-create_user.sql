32e35d58514910340ab27397e679037575425bb4
andreamarquez <andreamarquezg.95@gmail.com>
2025-03-06 20:43:41 +0100

    fix: script

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
--- This command creates the MySQL server user user_0d_1 with the password user_0d_1_pwd
+-- This command creates the MySQL server user user_0d_1 with the password user_0d_1_pwd if it does not already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- This command sets the password for user_0d_1 to user_0d_1_pwd
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- This command grants all privileges to the user user_0d_1 on localhost
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
