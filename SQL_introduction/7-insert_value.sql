1ce3097765ca63fcb1426ae277fc4f95d87e9f71
andreamarquez <andreamarquezg.95@gmail.com>
2025-03-06 17:00:13 +0100

    fix: script

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
- -- This inserts a new row into the table first_table
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+-- This inserts a new row into the table first_table
+INSERT INTO first_table SET id = 89, name = 'Best School';
