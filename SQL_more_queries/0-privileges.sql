58b7c61ba9506c986ac5477d41e1b82cd65d6eac
andreamarquez <andreamarquezg.95@gmail.com>
2025-03-06 20:33:45 +0100

    A script that lists all privileges of the MySQL users

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- This command lists all privileges of the MySQL user user_0d_1 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- This command lists all privileges of the MySQL user user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
