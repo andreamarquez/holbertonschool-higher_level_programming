06c651bf3a53fd143dcd263b1ca77be10117a5b1
andreamarquez <andreamarquezg.95@gmail.com>
2025-03-07 21:01:59 +0100

    fix: script

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,57 +1,24 @@
-#!/usr/bin/env python3
-"""
-This script lists all states from the database hbtn_0e_0_usa.
-
-Usage:
-    ./0-select_states.py <mysql_username> <mysql_password> <database_name>
-
-Arguments:
-    mysql_username: The MySQL username.
-    mysql_password: The MySQL password.
-    database_name: The name of the database to connect to.
-
-The script connects to a MySQL server running on localhost at port 3306,
-executes a query to retrieve all states sorted by id in ascending order,
-and prints each state.
-"""
-
+#!/usr/bin/python3
+'''
+This module  lists all states from the database hbtn_0e_0_usa
+'''
 import MySQLdb
 import sys
 
-def main():
-    """
-    Main function that retrieves and prints all states from the database.
-    """
-    # Get MySQL credentials from command line arguments
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    mysql_db = sys.argv[3]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_user,
-        passwd=mysql_password,
-        db=mysql_db
-    )
-
-    # Create a cursor object
-    cursor = db.cursor()
-
-    # Execute a query
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-
-    # Print the rows
-    for row in rows:
-        print(row)
-
-    # Close the cursor and connection
-    cursor.close()
-    db.close()
 
 if __name__ == "__main__":
-    main()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
