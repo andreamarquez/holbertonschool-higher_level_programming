54b22417898242190ec1406b85fdc4e9b6885129
andreamarquez <andreamarquezg.95@gmail.com>
2025-03-16 19:23:25 +0100

    fix: Script only returns the uppercase

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-This module  lists all states starting with N from the database hbtn_0e_0_usa
+This module lists all states starting with N from the database hbtn_0e_0_usa
 '''
 import MySQLdb
 import sys
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
+    db = MySQLdb.connect(host="127.0.0.1", port=3306,
                          user=username, passwd=password, db=database)
     cursor = db.cursor()
-    cursor.execute("SELECT *  FROM states "
-                   "WHERE name LIKE 'N%' "
-                   "ORDER BY id ASC ")
+    cursor.execute("SELECT * FROM states "
+                   "WHERE BINARY name LIKE 'N%' "
+                   "ORDER BY id ASC")
     states = cursor.fetchall()
 
     for state in states:
