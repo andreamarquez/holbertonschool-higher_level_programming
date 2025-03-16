1ab805d052a2db5c3c611d138c6fbd549c58dd29
andreamarquez <andreamarquezg.95@gmail.com>
2025-03-16 19:48:38 +0100

    fix: executable file

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,7 +2,7 @@
 """
 Script that takes in an argument and displays all values in the states
     table of hbtn_0e_0_usa where name matches the argument.
-    This version prevents SQL injection.
+    This version prevents SQL injection
 """
 import MySQLdb
 import sys
